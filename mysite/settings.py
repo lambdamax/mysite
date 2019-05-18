055d13c2deb32d971332178ba01a573db3ce2a17
yrh <370450848@qq.com>
2019-05-18 23:17:08 +0800

    调整media_root

--- mysite/settings.py
+++ mysite/settings.py
@@ -149,7 +149,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'blog/static'),
 ]
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = BASE_DIR
 MEDIA_URL = '/media/'
 
 # LOGGING = {
